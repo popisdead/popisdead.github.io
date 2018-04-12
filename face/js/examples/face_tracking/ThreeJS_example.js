8afd42ab0e25ca1687b2fede576b2e29e0add6af
Francesco Ferrazzino <ff@proxy42.com>
2018-04-12 23:08:28 +0200

    ccc

--- face/js/examples/face_tracking/ThreeJS_example.js
+++ face/js/examples/face_tracking/ThreeJS_example.js
@@ -58,7 +58,7 @@
 
 				// Set the 3D model according to the tracked results.
 
-				if(t3d) t3d.update(i, face, true);
+				if(t3d) t3d.update(i, face, true); 
 				document.getElementById("_t3d").style.width="100% !important";
 				document.getElementById("_t3d").style.height="auto !important";
 			}
